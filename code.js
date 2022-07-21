3c934a87aaaf14e27d81b206ace3719a57884493
Const <constantinarabadjiu@gmail.com>
2022-07-21 15:19:55 +0300

    Finished the project
    Included the ability to cancel the game
    
    New feature:the game doesn't continue until you type the pick correctly
    
    New feature:the game keeps track of the wins and loses and tells if the player lost or won at the end of the game

--- code.js
+++ code.js
@@ -1,1 +1,70 @@
-alert("hello")
+function getPlayerChoice(){
+    let imp=prompt("Pick rock,paper or scissors:","")
+    if(imp===null){ 
+        user=imp
+        return user
+    }else{
+        let input=imp.toLowerCase()
+        let user=input.trim()
+        console.log(user)
+        return user
+    }
+    
+    }
+    
+    function getComputerChoice(){
+        const choices=["rock","paper","scissors"]
+        let choice= choices[Math.floor(Math.random()*choices.length)]
+        console.log(choice)
+        return choice
+    }
+    
+    function round(){ 
+        let playerChoice=getPlayerChoice();
+        if(playerChoice===null){
+            alert("You canceled the game")
+            return "cancel"
+        }else{
+            let computerChoice=getComputerChoice();
+            if((playerChoice=="rock"&&computerChoice=="scissors")||(playerChoice=="scissors"&&computerChoice=="paper")||(playerChoice=="paper"&&computerChoice=="rock")){
+                return "You win"
+            }else if((playerChoice==="rock"&&computerChoice==="paper")||(playerChoice=="scissors"&&computerChoice=="rock")||(playerChoice=="paper"&&computerChoice=="scissors")){
+                return "You lose"
+            }else if((playerChoice==="rock"&&computerChoice==="rock")||(playerChoice=="scissors"&&computerChoice=="scissors")||(playerChoice=="paper"&&computerChoice=="paper")){
+                return "Draw"
+    
+            }else{
+                return "That pick is not available,choose again"
+            }
+        }
+        
+}
+
+let x=0;
+let y=0;
+
+for(i=1;i<=5;i++){
+    let result=round();
+    console.log(result);
+    if(result=="You win"){
+        x=x+1;
+    }else if(result=="You lose"){
+        y=y+1;
+    }else if(result=="That pick is not available,choose again"){
+        i--
+    }else if(result=="cancel"){
+        x=null
+        y=null
+        break
+    }
+}
+
+if(x>y){
+    console.log("You are the winner")
+}else if(x<y){
+    console.log("You are the loser")
+}else if(x=y){
+    console.log("It's a draw")
+}
+
+
